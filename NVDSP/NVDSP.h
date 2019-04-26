38476440a01bc0346f69c59e772bdaed02e215f3
Yakov Shteffen <shteffen@gmail.com>
2019-04-26 10:30:49 +0600

    Initial Commit

---
+++ NVDSP/NVDSP.h
@@ -0,0 +1,19 @@
+//
+//  NVDSP.h
+//  NVDSP
+//
+//  Created by Yakov Shteffen on 26/04/2019.
+//  Copyright © 2019 BlackBricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NVDSP.
+FOUNDATION_EXPORT double NVDSPVersionNumber;
+
+//! Project version string for NVDSP.
+FOUNDATION_EXPORT const unsigned char NVDSPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVDSP/PublicHeader.h>
+
+
